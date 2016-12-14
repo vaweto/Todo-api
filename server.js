c74798e8b4d23aee4e13c339ba4804d01a631e58
Vagelis Bismpikis <vagelisbisbikis@gmail.com>
2016-12-14 19:49:51 +0200

    use underscore js on project

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 // this is our model and 
 //  all todos is our collection
-var todos = [{
-	id: 1,
-	description: 'MEET mof for lunch',
-	completed: false
-},{
-	id: 2,
-	description: 'Go to market',
-	completed: false
-},{
-	id: 3,
-	description: 'Go for walk',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/',function (req, res) {
 	res.send('Todo Api Root');
@@ -26,16 +20,19 @@ app.get('/todos',function (req, res) {
 	res.json(todos);
 });
 
+
+//Get todo by id
 app.get('/todos/:id',function (req, res) {
 
 	var todoId = parseInt(req.params.id);
-	var matcheTodo;
+	var matcheTodo = _.findWhere(todos, {id: todoId});
+	// var matcheTodo;
 
-	todos.forEach ( function(todo) {
-		if (todo.id === todoId) {
-			matcheTodo = todo;
-		} 
-	});
+	// todos.forEach ( function(todo) {
+	// 	if (todo.id === todoId) {
+	// 		matcheTodo = todo;
+	// 	} 
+	// });
 
 	if(matcheTodo) {
 		res.json(matcheTodo);
@@ -43,6 +40,28 @@ app.get('/todos/:id',function (req, res) {
 		res.status(404).send();
 	}
 	//res.send(req.params.id);
+});
+
+//Post a new todo
+app.post('/todos', function(req, res) {
+	
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if ( !_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
+		
+		return res.status(400).send();
+
+	} else {
+
+		body.description = body.description.trim();
+
+		body.id = todoNextId++;
+
+		todos.push(body);
+
+		res.json(body);
+	}
+ 
 });
 
 app.listen(PORT, function() {
