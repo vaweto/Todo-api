bc1f8a1bc96c3a7c72ee20e1c3d5271eec951892
Vagelis Bismpikis <vagelisbisbikis@gmail.com>
2016-12-16 21:45:36 +0200

    add completed filterd

--- server.js
+++ server.js
@@ -17,7 +17,18 @@ app.get('/',function (req, res) {
 });
 
 app.get('/todos',function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos,{completed:true});
+	} else if( queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos,{completed:false});
+	}
+
+
+	res.json(filteredTodos);
 });
 
 
